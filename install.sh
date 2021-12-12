d99b091802caaf57a415b6e7981ddfae6a3d5835
Snowyowl <rikugamesion@gmail.com>
2021-12-12 15:20:41 +0900

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+
+sudo apt-get install binutils
+sudo apt-get install xxd
+sudo apt-get install file
+sudo apt update
+
+wget "https://github.com/crum7/Stella/raw/main/stella01"
+mv stella01 stella
+chmod 777 stella
+

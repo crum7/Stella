841ac1af1ef9f710a1eb88bc5da08772eb3d6796
Snowyowl <rikugamesion@gmail.com>
2021-12-12 15:21:28 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,9 @@ wget "https://github.com/crum7/Stella/raw/main/stella01"
 mv stella01 stella
 chmod 777 stella
 
+wget "https://github.com/fireeye/flare-floss/releases/download/v1.7.0/floss-v1.7.0-linux.zip"
+unzip floss-v1.7.0-linux.zip
+wget "https://github.com/crum7/stella/raw/main/stellaf01"
+mv stellaf01 stellaf
+chmod 777 floss
+chmod 777 stellaf

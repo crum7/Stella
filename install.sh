3e9e7c58b07437790cf689412cb4c0af8dca41a3
Snowyowl <rikugamesion@gmail.com>
2021-12-14 18:05:13 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,15 @@ sudo apt-get install binutils -y
 sudo apt-get install xxd -y
 sudo apt-get install file -y
 sudo apt-get install figlet -y
+cd /usr/local/bin
 
-wget "https://github.com/crum7/Stella/raw/main/stella01"
-mv stella01 stella
+wget "https://github.com/crum7/Stella/raw/main/stella2.0"
+mv stella2.0 stella
 chmod 777 stella
 
 wget "https://github.com/fireeye/flare-floss/releases/download/v1.7.0/floss-v1.7.0-linux.zip"
 unzip floss-v1.7.0-linux.zip
-wget "https://github.com/crum7/stella/raw/main/stellaf01"
-mv stellaf01 stellaf
+
 chmod 777 floss
-chmod 777 stellaf
 rm floss-v1.7.0-linux.zip
 rm install.sh
